806f2a66c9773dd1f48bcd3515a9c319051ba2b1
Allen Chan <allenzery@hotmail.com>
2017-11-22 23:21:15 -0800

    Changing home page

--- src/assets/views/HomeView.js
+++ src/assets/views/HomeView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Button } from 'semantic-ui-react';
 
 const backgroundImage = 'https://images.pexels.com/photos/163848/road-mountains-sunset-path-163848.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb';
 const style = {
@@ -22,8 +23,10 @@ export default class Home extends React.Component {
           <div>
             <Navbar/>
             <div style = {backgroundStyle}>
+              <span style = {{fontSize: '80px', fontFamily: 'futura', marginTop: '120px', position: 'absolute'}}>Vantage</span>
+              <span style = {{fontSize: '40px', fontFamily: 'futura', marginTop: '190px', position: 'absolute'}}>The Ultimate Car Recommendation Engine</span>
               <div style = {{alignSelf: 'center'}}>
-                <RaisedButton label="Click here to get started with your recommendation" secondary={true} style={style} onClick={()=>{this.context.router.push('/recommend');}}/>
+                <Button size="huge" color='teal' onClick={()=>{this.context.router.push('/recommend')}}>Click here to get started with your recommendation</Button>
               </div>
             </div>
           </div>
